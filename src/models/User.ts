1b608c5e7a5f3fd385f308035668b87aeb1f3c9c
MultipleIllusionsi <seregaserega06@gmail.com>
2020-03-03 00:28:13 +0500

    added new model

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,6 +1,7 @@
 import { Eventing } from "./Eventing";
 import { Sync } from "./Sync";
 import { Attributes } from "./Attributes";
+import { AxiosResponse } from "axios";
 
 const rootUrl = "http://localhost:3000/users";
 
@@ -17,5 +18,45 @@ export class User {
 
   constructor(attrs: UserProps) {
     this.attributes = new Attributes<UserProps>(attrs);
+  }
+
+  get on() {
+    return this.events.on;
+  }
+
+  get trigger() {
+    return this.events.trigger;
+  }
+
+  get get() {
+    return this.attributes.get;
+  }
+
+  set(update: UserProps): void {
+    this.attributes.set(update);
+    this.events.trigger("change");
+  }
+
+  fetch(): void {
+    const id = this.get("id");
+
+    if (typeof id !== "number") {
+      throw new Error("Cannot fetch without an id");
+    }
+
+    this.sync.fetch(id).then((res: AxiosResponse): void => {
+      this.set(res.data);
+    });
+  }
+
+  save(): void {
+    this.sync
+      .save(this.attributes.getAll())
+      .then((res: AxiosResponse): void => {
+        this.trigger("save");
+      })
+      .catch(() => {
+        this.trigger("error");
+      });
   }
 }
